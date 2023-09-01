44599955d60510b02c6266853ae8cac3c07623eb
David Tai <didavid61202@gmail.com>
2023-09-01 16:06:29 +0800

    fix: proxy client close event

--- src/middleware/web-incoming.ts
+++ src/middleware/web-incoming.ts
@@ -154,6 +154,9 @@ const stream = defineProxyMiddleware(
           server.emit("end", req, res, proxyRes);
         }
       } else {
+        res.on('close', function () {
+          proxyRes.destroy()
+        });
         // Allow us to listen when the proxy has completed
         proxyRes.on("end", function () {
           if (server) {
